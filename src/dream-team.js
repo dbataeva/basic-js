fe83b1e09f579568b1b88b90d809eb7bb83482d9
dbataeva <b.daffa.v@yandex.ru>
2023-04-15 21:48:21 +0300

    feat: dream team is done (Сб 15 апр 2023 21:48:21 MSK)

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const teamName = members.map(name => {
+    if (typeof name !== 'string') {
+      return ;
+    }
+    const trimmedName = name.trim();
+
+    return trimmedName[0].toUpperCase();
+  });
+
+  teamName.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
+
+  return teamName.join('');
 }
 
 module.exports = {

1773b5f6206f816c57cb3ebd272590b036542ac8
dbataeva <b.daffa.v@yandex.ru>
2023-04-16 13:14:41 +0300

    feat: transform array is done (Вс 16 апр 2023 13:14:41 MSK)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const discardNext = '--discard-next'; //excludes the next element of the array from the transformed array
+const discardPrev = '--discard-prev'; //excludes the previous element of the array from the transformed array
+const doubleNext = '--double-next'; //duplicates the next element of the array in the transformed array
+const doublePrev = '--double-prev'; //duplicates the previous element of the array in the transformed array
+
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let lastCommand;
+  const res = [];
+
+  arr.forEach((elem, index, arr) => {
+    if (elem === discardNext) {
+      lastCommand = discardNext;
+    } else if (elem === discardPrev) {
+      if (arr[index - 2] !== discardNext) {
+        res.pop();
+      }
+      lastCommand = discardPrev;
+    } else if (elem === doubleNext ) {
+      if (arr[index + 1]) {
+        res.push(arr[index + 1]);
+      }
+      lastCommand = doubleNext
+    } else if (elem === doublePrev) {
+      if (arr[index - 1] && arr[index - 2] !== discardNext) {
+        res.push(arr[index - 1]);
+      }
+      lastCommand = doublePrev;
+    } else if (lastCommand === discardNext) {
+      lastCommand = 'nothing';
+    } else {
+      res.push(elem);
+      lastCommand = 'push';
+    }
+  });
+
+  return res;
 }
 
 module.exports = {
